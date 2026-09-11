be6c1d9a4c3902d892f1dd0927c4f45a15e09a5b
Wooseok Yang <huan.sock@gmail.com>
2026-09-11 22:24:20 +0200

    tree-sitter 언어별 설정 추가

--- dot_config/nvim/ftplugin/elixir.lua
+++ dot_config/nvim/ftplugin/elixir.lua
@@ -1,6 +1,3 @@
-if vim.b.did_ftplugin then
-  return
-end
-vim.b.did_ftplugin = 1
-
 vim.lsp.enable('elixirls')
+
+vim.treesitter.start()
--- dot_config/nvim/ftplugin/rust.lua
+++ dot_config/nvim/ftplugin/rust.lua
@@ -1,6 +1,3 @@
-if vim.b.did_ftplugin then
-  return
-end
-vim.b.did_ftplugin = 1
-
 vim.lsp.enable('rust_analyzer')
+
+vim.treesitter.start()
