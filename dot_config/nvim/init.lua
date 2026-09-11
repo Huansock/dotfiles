55c5ad2d53b576f3be5d078701d13c568b55efa3
Wooseok Yang <huan.sock@gmail.com>
2026-09-11 22:36:41 +0200

    option을 추가함

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,15 +1,11 @@
+require('options')
+
 -- vim.pack 문서 https://neovim.io/doc/user/pack/#vim.pack
-
--- file type plugin 문서 https://neovim.io/doc/user/filetype/#_2.-filetype-plugin
-
 vim.pack.add({
-  -- lsp 설정 파일
   'https://github.com/neovim/nvim-lspconfig' ,
-  -- coq 자동완성
   { src = "https://github.com/ms-jpq/coq_nvim" , version = 'coq'},
   { src = "https://github.com/ms-jpq/coq.artifacts" , version = 'artifacts'},
   { src = "https://github.com/ms-jpq/coq.thirdparty" , version = '3p'},
-  -- treesitter
   "https://github.com/nvim-treesitter/nvim-treesitter",
   {src = "https://github.com/catppuccin/nvim", name = "catppuccin"}
   
