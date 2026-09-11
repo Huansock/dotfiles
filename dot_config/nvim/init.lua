2bd0e45d7434785ec82c62709cd669f3b2e0c3a5
Wooseok Yang <huan.sock@gmail.com>
2026-09-11 21:49:39 +0200

    정리했지요.

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -2,29 +2,22 @@
 
 -- file type plugin 문서 https://neovim.io/doc/user/filetype/#_2.-filetype-plugin
 
-local gh = function(x) return 'https://github.com/' .. x end
-
 vim.pack.add({
   -- lsp 설정 파일
-  gh('neovim/nvim-lspconfig') ,
+  'https://github.com/neovim/nvim-lspconfig' ,
   -- coq 자동완성
-  { src = gh("ms-jpq/coq_nvim") , version = 'coq'},
-  { src = gh("ms-jpq/coq.artifacts") , version = 'artifacts'},
-  { src = gh("ms-jpq/coq.thirdparty") , version = '3p'},
+  { src = "https://github.com/ms-jpq/coq_nvim" , version = 'coq'},
+  { src = "https://github.com/ms-jpq/coq.artifacts" , version = 'artifacts'},
+  { src = "https://github.com/ms-jpq/coq.thirdparty" , version = '3p'},
   -- treesitter
-  gh("nvim-treesitter/nvim-treesitter") ,
-  {src = gh("catppuccin/nvim"), name = "catppuccin"}
+  "https://github.com/nvim-treesitter/nvim-treesitter",
+  {src = "https://github.com/catppuccin/nvim", name = "catppuccin"}
   
 })
 
 vim.g.coq_settings = {}
 
 require('coq')
-
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = { '<filetype>' },
-  callback = function() vim.treesitter.start() end,
-})
 
 vim.cmd("colorscheme catppuccin-frappe")
 
