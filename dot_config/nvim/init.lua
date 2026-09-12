b79abf1d9a1f03832a93eeee26fb4bd2ba5660a5
Wooseok Yang <huan.sock@gmail.com>
2026-09-12 22:33:09 +0200

    add mini starter

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -8,6 +8,7 @@ vim.pack.add({
 	{ src = "https://github.com/catppuccin/nvim", name = "catppuccin" },
 	'https://github.com/stevearc/oil.nvim.git',
 	'https://github.com/nvim-mini/mini.icons',
+	'https://github.com/nvim-mini/mini.starter',
 	{ src = 'https://github.com/ms-jpq/coq_nvim', version = 'coq' },
 	{ src = 'https://github.com/ms-jpq/coq.artifacts', version = 'artifacts' },
 	{ src = 'https://github.com/ms-jpq/coq.thirdparty', version = '3p' },
@@ -38,5 +39,24 @@ FzfLua.register_ui_select({
 		width = 0.50
 	}
 })
+
+local starter = require('mini.starter')
+starter.setup({
+	evaluate_single = true,
+	items = {
+		starter.sections.recent_files(5, true),
+		starter.sections.recent_files(5, false),
+		{ name = 'Files (Fzf)', action = 'FzfLua files', section = 'Actions' },
+		{ name = 'Grep (Fzf)', action = 'FzfLua live_grep', section = 'Actions' },
+		{ name = 'Explorer (Oil)', action = 'Oil', section = 'Actions' },
+		{ name = 'New buffer', action = 'enew', section = 'Builtin actions' },
+		{ name = 'Quit', action = 'qall', section = 'Builtin actions' },
+	},
+	content_hooks = {
+		starter.gen_hook.adding_bullet(),
+		starter.gen_hook.padding(3, 2),
+	},
+})
+
 
 
